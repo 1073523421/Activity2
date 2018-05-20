e5b98235d4b91434973797a2244f0bcc36af5b83
zkx <1073523421@qq.com>
2018-05-20 17:22:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zkx/activity2/MButton.java
@@ -0,0 +1,22 @@
+package com.example.zkx.activity2;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Button;
+
+
+
+public class MButton extends android.support.v7.widget.AppCompatButton {
+    public MButton(Context context , AttributeSet set){
+        super(context , set);
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        super.onKeyDown(keyCode , event);
+        Log.v("com.sise" , "the onKeyDown in MyButton");
+        //返回true，表明该事件不会向外扩散
+        return true;
+    }
+}
